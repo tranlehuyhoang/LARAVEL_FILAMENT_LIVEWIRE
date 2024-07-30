64fb781acf8c55b9423c38d54125005754f6fd80
Huyh Huyhu <2509roblox@gmail.com>
2024-07-30 10:23:40 +0700

    filament orders

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -6,10 +6,10 @@ use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
 use App\Models\Product;
-use Faker\Core\Number;
 use Filament\Forms;
 use Filament\Forms\Components\Group;
 use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
@@ -22,12 +22,13 @@ use Filament\Forms\Get;
 use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
-use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Number as SupportNumber;
+use Illuminate\Support\Number;
 
 class OrderResource extends Resource
 {
@@ -40,9 +41,9 @@ class OrderResource extends Resource
         return $form
             ->schema([
                 Group::make()->schema([
-                    Section::make('Order Information')->schema([
+                    Section::make("Order Infomation")->schema([
                         Select::make('user_id')
-                            ->label('Customer')
+                            ->label("Customer")
                             ->relationship('user', 'name')
                             ->searchable()
                             ->preload()
@@ -50,96 +51,79 @@ class OrderResource extends Resource
 
                         Select::make('payment_method')
                             ->options([
-                                'stripe' => 'Stripe',
-                                'cod' => 'Cash on Delivery',
-                            ])
-                            ->required(),
+                                'stripe' => "Strip",
+                                'cod' => "Cash on Delivery",
+                            ])->required()
+                            ->preload()
+                            ->searchable(),
 
                         Select::make('payment_status')
                             ->options([
-                                'pending' => 'Pending',
-                                'paid' => 'Paid',
-                                'failed' => 'Failed',
+                                'pending' => "Pending",
+                                'paid' => "Paid",
+                                'failed' => "Failed",
                             ])
                             ->default('pending')
                             ->required(),
-
                         ToggleButtons::make('status')
                             ->inline()
+                            ->default('new')
+                            ->required()
                             ->options([
-                                'new' => 'New',
-                                'processing' => 'Processing',
-                                'shipped' => 'Shipped',
-                                'delivered' => 'Delivered',
-                                'canceled' => 'Canceled',
-                            ])
-                            ->icons([
-                                'new' => 'heroicon-s-plus-circle',
-                                'processing' => 'heroicon-s-plus-circle',
-                                'shipped' => 'heroicon-s-truck',
-                                'delivered' => 'heroicon-s-check-circle',
-                                'canceled' => 'heroicon-s-x-circle',
+                                'new' => "New",
+                                'processing' => "Processing",
+                                'shipped' => "Shipped",
+                                'delivered' => "Delivered",
+                                'cancelled' => "Cancelled",
                             ])
                             ->colors([
-                                'new' => 'primary',
-                                'processing' => 'warning',
-                                'shipped' => 'info',
-                                'delivered' => 'success',
-                                'canceled' => 'danger',
+                                'new' => "info",
+                                'processing' => "warning",
+                                'shipped' => "success",
+                                'delivered' => "success",
+                                'cancelled' => "danger",
                             ])
-                            ->default('new')
-                            ->required(),
+                            ->icons([
+                                'new' => "heroicon-o-rectangle-stack",
+                                'processing' => "heroicon-o-rectangle-stack",
+                                'shipped' => "heroicon-o-rectangle-stack",
+                                'delivered' => "heroicon-o-rectangle-stack",
+                                'cancelled' => "heroicon-o-rectangle-stack",
+                            ]),
 
-                        // Currency Selection
                         Select::make('currency')
                             ->options([
-                                'usd' => 'USD - US Dollar',
-                                'eur' => 'EUR - Euro',
-                                'gbp' => 'GBP - British Pound',
-                                'jpy' => 'JPY - Japanese Yen',
-                                'aud' => 'AUD - Australian Dollar',
+                                'vnd' => "VND",
+                                'usd' => "USD",
                             ])
+                            ->default('vnd')
                             ->required(),
 
-                        // Shipping Method Selection
                         Select::make('shipping_method')
                             ->options([
-                                'standard' => 'Standard Shipping',
-                                'express' => 'Express Shipping',
-                                'overnight' => 'Overnight Shipping',
+                                'ghn' => "Giao Hang Nhanh",
+                                "express" => "Express"
                             ])
+                            ->default('ghn')
                             ->required(),
-
-                        // Notes Textarea
-                        Textarea::make('notes')
-                            ->placeholder('Enter any additional notes here...')
-                            ->rows(5),
-
-
+                        MarkdownEditor::make('notes')
+                            ->fileAttachmentsDirectory('notes')
                     ])->columns(2),
 
-                    Section::make('Order Items')->schema([
-                        Repeater::make('items')
-                            ->relationship()
+                    Section::make("Order Items")->schema([
+                        Repeater::make('orderItems') // tên trường cho repeater + relationship
+                            ->relationship() //relationship() giúp Filament hiểu rằng các mục trong Repeater là các bản ghi con của mô hình chính và cần được xử lý theo cách đó.
                             ->schema([
                                 Select::make('product_id')
                                     ->relationship('product', 'name')
                                     ->searchable()
-                                    ->preload()
                                     ->required()
+                                    ->preload()
                                     ->distinct()
                                     ->disableOptionsWhenSelectedInSiblingRepeaterItems()
-                                    ->columnSpan(4)
-                                    ->reactive()
-                                    ->afterStateUpdated(function ($state, $get, $set) {
-                                        // Update `unit_amount` and `total_amount` when `product_id` changes
-                                        $product = Product::find($state);
-                                        if ($product) {
-                                            $set('unit_amount', $product->price); // Assuming `price` is the column name for product price
-                                            $quantity = $get('quantity');
-                                            $set('total_amount', $product->price * $quantity);
-                                        }
-                                    }),
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', Product::find($state)?->price * $get('quantity') ?? 0))
+                                    ->columnSpan(4),
 
                                 TextInput::make('quantity')
                                     ->numeric()
@@ -147,74 +131,94 @@ class OrderResource extends Resource
                                     ->default(1)
                                     ->minValue(1)
                                     ->reactive()
-                                    ->columnSpan(2)
-                                    ->afterStateUpdated(function ($state, $get, $set) {
-                                        $unitAmount = $get('unit_amount');
-                                        $set('total_amount', $unitAmount * $state);
-                                    }),
-
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount')))
+                                    ->columnSpan(2),
                                 TextInput::make('unit_amount')
                                     ->numeric()
                                     ->required()
                                     ->disabled()
+                                    ->dehydrated()
                                     ->columnSpan(3),
                                 TextInput::make('total_amount')
                                     ->numeric()
-                                    ->disabled()
                                     ->required()
-                                    ->columnSpan(3)
+                                    ->columnSpan(3),
                             ])->columns(12),
-                        Placeholder::make('grand_total_placeholder')
+                        Placeholder::make('grand_total_placecholder')
                             ->label('Grand Total')
                             ->content(function (Get $get, Set $set) {
                                 $total = 0;
-
-                                // Retrieve the items repeater data
-                                $repeaters = $get('items');
-
-                                // Check if there are items
-                                if ($repeaters) {
+                                if ($repeaters = $get('orderItems')) {
                                     foreach ($repeaters as $key => $repeater) {
-                                        // Sum the total_amount of each item
-                                        $total += $get("items.{$key}.total_amount");
+                                        $total += $get("orderItems.{$key}.total_amount");
                                     }
                                 }
                                 $set('grand_total', $total);
-                                // Return the formatted total amount with currency
-                                return SupportNumber::currency($total, 'INR');
+                                return Number::currency($total, 'VND');
                             }),
+                        Hidden::make('grand_total')
+                            ->default(0),
+
+
+
+
                     ]),
-                    Hidden::make('grand_total')->default(0),
 
+                ])->columnSpanFull()
 
-                ])->columnSpanFull(),
             ]);
     }
+
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('user.name')
+                TextColumn::make('user.name') //user relationship
                     ->label('Customer')
                     ->sortable()
                     ->searchable(),
                 TextColumn::make('grand_total')
                     ->numeric()
                     ->sortable()
-                    ->money('INR'),
+                    ->money('VND'),
                 TextColumn::make('payment_method')
-                    ->searchable()
-                    ->sortable(),
+                    ->sortable()
+                    ->searchable(),
                 TextColumn::make('payment_status')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('currency')
                     ->searchable()
                     ->sortable(),
+                TextColumn::make('shipping_method')
+                    ->searchable()
+                    ->sortable(),
+                SelectColumn::make('status')
+                    ->options([
+                        'new' => "New",
+                        'processing' => "Processing",
+                        'shipped' => "Shipped",
+                        'delivered' => "Delivered",
+                        'cancelled' => "Cancelled",
+                    ]),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated-at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+
+
             ])
-            ->filters([
-                //
-            ])
+            ->filters([])
             ->actions([
-                Tables\Actions\ViewAction::make(),
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -229,7 +233,14 @@ class OrderResource extends Resource
             //
         ];
     }
-
+    public static function getNavigationBadge(): ?string
+    {
+        return  static::getModel()::count();
+    }
+    public static function getNavigationBadgeColor(): string|array|null
+    {
+        return  static::getModel()::count() > 10 ? 'danger' : 'success';
+    }
     public static function getPages(): array
     {
         return [
